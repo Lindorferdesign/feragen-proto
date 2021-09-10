69b1e5899bfec0eed45a498f888137332b7fc7cf
Daniel Lindorfer <daniel@lindorferdesign.com>
2021-09-10 13:21:25 +0200

    setup

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Head from 'next/head'
+import BannerTeaser from "../Components/BannerTeaser"
 
 export default function Home() {
   return (
+    <div>
+    <BannerTeaser></BannerTeaser>
+
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+
+      
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
@@ -77,6 +84,7 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
+    </div>
     </div>
   )
 }
